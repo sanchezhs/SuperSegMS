b6c3030b8d78115eddbc8f111ece032ee8587b55
sanchezhs <ssancheztoca@gmail.com>
2025-02-19 08:22:08 +0100

    pipeline 2

--- pipeline.py
+++ pipeline.py
@@ -1,76 +1,38 @@
-import yaml
-from enum import StrEnum
+import argparse
+import json
 
-from preprocess.preprocess import create_pipeline_from_config
+from preprocess.preprocess import apply_preprocessing
 
-class Steps(StrEnum):
-    PREPROCESSING = "preprocessing"
-    TRAINING = "training"
-    EVALUATION = "evaluation"
-    PREDICTIONS = "prediction"
-
-def main(config: dict) -> None:
-    print(config)
-
-    pipeline = create_pipeline_from_config(config)
-    steps = [
-        x.get('name') for x in config.get('steps', [])
-    ]
-
-    if Steps.PREPROCESSING in steps:
-        print("Preprocesamiento")
-        pipeline.process()
-    if Steps.TRAINING in steps:
-        print("Entrenamiento")
-    if Steps.EVALUATION in steps:
-        print("Evaluación")
-    if Steps.PREDICTIONS in steps:
-        print("Predicciones")
-
-    # pipeline.process()
-
-    # 1. Preprocesamiento
-    # if config["preprocessing"]["method"] == "fsrcnn":
-    #     print("Aplicando super-resolución con FSRCNN...")
-    #     enhance_images()
-    # elif config["preprocessing"]["method"] == "basic":
-    #     print("Aplicando preprocesado básico...")
-    #     preprocess_images()
-    # else:
-    #     print("No se aplicará preprocesado adicional.")
-
-    # # 2. Preparación del dataset
-    # print("Preparando dataset...")
-    # prepare_dataset()
-
-    # # 3. Entrenamiento del modelo
-    # model_type = config["model"]["type"]
-    # if model_type == "yolo":
-    #     print("Entrenando modelo YOLO...")
-    #     train_yolo()
-    # elif model_type == "unet":
-    #     print("Entrenando modelo UNet...")
-    #     train_unet()
-    # else:
-    #     raise ValueError(f"Modelo '{model_type}' no reconocido.")
-
-    # # 4. Evaluación del modelo
-    # print("Evaluando modelo...")
-    # evaluate_model(model_type)
-
-    # # 5. Predicciones
-    # print("Realizando predicciones...")
-    # make_predictions(model_type)
-
-def load_config() -> dict:
-    try:
-        with open("config.yaml", "r") as f:
-            config = yaml.safe_load(f)
-        return config
-    except FileNotFoundError:
-        print("No se ha encontrado el archivo de configuración 'config.yaml'.")
-        sys.exit(1)
+def load_config(config_path: str) -> dict:
+    with open(config_path, "r") as f:
+        config = json.load(f)
+    return config
 
 if __name__ == "__main__":
-    config = load_config()
-    main(config)
+    parser = argparse.ArgumentParser(description="TFM Pipeline")
+    parser.add_argument("--step", type=str, required=True, choices=["preprocess", "train", "evaluate", "predict"])
+    parser.add_argument("--model", type=str, required=True, choices=["unet", "yolo"])
+    parser.add_argument("--config", type=str, default="config.json",  help="Path to configuration file")
+
+    args = parser.parse_args()
+
+    config = load_config(args.config)
+
+    parts = config["hyperparameters"]["img_size"].split("x")
+    img_size = (int(parts[0]), int(parts[1]))
+
+    if args.step == "preprocess":
+        apply_preprocessing(
+            args.model,
+            img_size,
+            float(config["hyperparameters"]["split"]),
+            config["dataset"]["input_path"], 
+            config["dataset"]["output_path"]
+        )
+    elif args.step == "train":
+        raise NotImplementedError("Train step not implemented")
+    elif args.step == "evaluate":
+        raise NotImplementedError("Evaluate step not implemented")
+    elif args.step == "predict":
+        raise NotImplementedError("Predict step not implemented")
+
---
+++ utils/logger.py
@@ -0,0 +1,24 @@
+from logging import getLogger, StreamHandler, Formatter, DEBUG, INFO
+import sys
+
+def get_logger(name: str, level: int = INFO):
+    logger = getLogger(name)
+    logger.setLevel(level)
+    logger.propagate = False  # Prevent propagation to parent loggers
+    
+    # Remove any existing handlers
+    logger.handlers.clear()
+    
+    handler = StreamHandler(stream=sys.stdout)
+    handler.setLevel(level)
+    formatter = Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    return logger
+
+if __name__ == "__main__":
+    logger = get_logger("test_logger", level=DEBUG)
+    logger.debug("Debug message")
+    logger.info("Info message")
+    logger.warning("Warning message")
+    logger.error("Error message")
