ed98aa66c53f15b5c683e7797773f8d61afb8495
sanchezhs <ssancheztoca@gmail.com>
2025-06-01 15:40:52 +0200

    updated send msg

--- utils/send_msg.py
+++ utils/send_msg.py
@@ -16,19 +16,23 @@ def send_whatsapp_message(message: str) -> None:
     Raises:
         ValueError: If required environment variables are not set.
     """
-    phone_number = os.getenv("PHONE_NUMBER", None)
-    if not phone_number:
-        raise ValueError("PHONE_NUMBER environment variable is not set.")
+    try:
+        phone_number = os.getenv("PHONE_NUMBER", None)
+        if not phone_number:
+            raise ValueError("PHONE_NUMBER environment variable is not set.")
 
-    api_key = os.getenv("CMB_API_KEY", None)
-    if not api_key:
-        raise ValueError("CMB_API_KEY environment variable is not set.")
+        api_key = os.getenv("CMB_API_KEY", None)
+        if not api_key:
+            raise ValueError("CMB_API_KEY environment variable is not set.")
 
-    url = f"https://api.callmebot.com/whatsapp.php?phone={phone_number}&text={urllib.parse.quote(message)}&apikey={api_key}"
+        url = f"https://api.callmebot.com/whatsapp.php?phone={phone_number}&text={urllib.parse.quote(message)}&apikey={api_key}"
 
-    response = requests.get(url)
+        response = requests.get(url)
 
-    if response.status_code == 200:
-        logger.info("Message sent successfully via WhatsApp.")
-    else:
-        logger.error(f"Error al enviar el mensaje: {response}")
+        if response.status_code == 200:
+            logger.info("Message sent successfully via WhatsApp.")
+        else:
+            logger.error(f"Error al enviar el mensaje: {response}")
+    except Exception as e:
+        logger.error(f"Failed to send WhatsApp message: {e}")
+        
